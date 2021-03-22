778401c8bebd40757c60fb91e99172269bfbfbbf
jel-0 <jibri.lewis01@gmail.com>
2021-03-22 15:16:01 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Slave Instagram account credentials 
+usr = ""
+instagram_passowrd = ""
+
+#Path to Chrome webdriver for selium
+PATH = "C:\chromedriver.exe"
+
+# Instagram account that you want to Monitor
+trgact = ""
+
+#callmebot.com free whatspp Messageing API Credentials
+    #Example "phone=+12645839999"
+whtsappnum = ""
+    #Example "&apikey=177844"
+whtsappapi = ""
